d2718ae6dcec0c64101bc0ef68aefb5583b46fe9
coronadito <alejandra.coronadojl@gmail.com>
2021-09-27 20:38:38 -0500

    cambios hasta el 27 de septiembre

--- ProyectoCiclo3.App.Frontend/Pages/Estaciones/List.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Estaciones/List.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using ProyectoCiclo3.App.Dominio;
  
 namespace ProyectoCiclo3.App.Frontend.Pages
 {
     public class ListEstacionModel : PageModel
     {
+            private readonly RepositorioEstaciones repositorioEstaciones;
+            public IEnumerable<Estaciones> Estaciones { get; set; }
+
+        public ListEstacionModel(RepositorioEstaciones repositorioEstaciones)
+        {
+            this.repositorioEstaciones=repositorioEstaciones;
+        }
+
         public void OnGet()
         {
- 
+            Estaciones =repositorioEstaciones.GetAll();
         }
     }
 }
---
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioRutas.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using ProyectoCiclo3.App.Dominio;
+using System.Linq;
+using System;
+ 
+namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
+{
+    public class RepositorioRutas
+    {
+        List<Rutas> rutas; 
+ 
+    public RepositorioRutas()
+        {
+            rutas= new List<Rutas>()
+            {
+                new Rutas{id=1,origen=101,destino= 201,tiempo_estimado= 30},
+                new Rutas{id=2,origen=102,destino= 202,tiempo_estimado= 40},
+                new Rutas{id=3,origen=103,destino= 203,tiempo_estimado= 50}
+                
+            };
+        }
+ 
+        public IEnumerable<Rutas> GetAll()
+        {
+            return rutas;
+        }
+ 
+        public Rutas GetRutaWithId(int id){
+            return rutas.SingleOrDefault(b => b.id == id);
+        }
+    }
+}
