72ffbd0d6daa239dd0ba13878893f1c1ca979e5a
coronadito <alejandra.coronadojl@gmail.com>
2021-10-04 20:17:58 -0500

    cambios hasta el 4 de octubre

--- ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Buses.cs
@@ -1,11 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ProyectoCiclo3.App.Dominio{
     public class Buses{
+        
         public int id {get; set;}
+        [Required]
         public string marca {get; set;} 
         public int modelo {get; set;} 
         public int kilometraje {get; set;} 
         public int numero_asientos {get; set;} 
         public string placa {get; set;}
     }
-}
+}   
